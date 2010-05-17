2cb8b05bdd3e18807531ca30d298ccb4db6aef72
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2010-05-17 17:03:22 -0300

    Solved Bug #312514

--- examples/org.eclipse.sketch.examples.shapes.diagram/src/org/eclipse/sketch/examples/shapes/diagram/application/DiagramEditorWorkbenchWindowAdvisor.java
+++ examples/org.eclipse.sketch.examples.shapes.diagram/src/org/eclipse/sketch/examples/shapes/diagram/application/DiagramEditorWorkbenchWindowAdvisor.java
@@ -13,6 +13,10 @@
  */ 
  package org.eclipse.sketch.examples.shapes.diagram.application;
 
+import java.io.File;
+
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.sketch.examples.shapes.diagram.part.Messages;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.ActionBarAdvisor;
@@ -50,4 +54,42 @@ public class DiagramEditorWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor 
 		configurer.setTitle(Messages.DiagramEditorWorkbenchWindowAdvisor_Title);
 	}
 
+	@Override
+	public void postWindowClose() {
+		// TODO Auto-generated method stub
+		super.postWindowClose();
+
+		this.cleanGalleryTempFiles();
+
+	}
+
+	private void cleanGalleryTempFiles() {
+		System.out.println("Cleaning temporary files...");
+
+		IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace()
+				.getRoot();
+
+		File dir = myWorkspaceRoot.getLocation().toFile();
+
+		if (!dir.exists()) {
+			System.out.println(myWorkspaceRoot.getFullPath().toFile()
+					+ " does not exist");
+			return;
+		}
+		String patt = ".svg";
+
+		String[] info = dir.list();
+		for (int i = 0; i < info.length; i++) {
+			File n = new File(dir + dir.separator + info[i]);
+			if (!n.isFile()) { // skip ., .., other directories, etc.
+				continue;
+			}
+			if (info[i].indexOf(patt) == -1) { // name doesn't match
+				continue;
+			}
+			// System.out.println("removing " + n.getPath());
+			if (!n.delete())
+				System.err.println("Couldn't remove " + n.getPath());
+		}
+	}
 }
